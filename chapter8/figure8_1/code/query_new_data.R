ebc1c4f9fddfcbf0215dfbaa2ccc2ce299c1792e
Matt Moehr <mattmoehr@gmail.com>
2018-02-12 19:05:25 -0600

    first draft of getting updated data

--- chapter8/figure8_1/code/query_new_data.R
+++ chapter8/figure8_1/code/query_new_data.R
@@ -7,4 +7,22 @@ anes <- readRDS("./chapter8/figure8_1/new/data/anes/2016 Time Series Study/anes_
 
 ls(anes)
 
+## welfare: V161209
+  ## 0 = increase (1)
+  ## 50 = maintain (3)
+  ## 100 = decrease (2)
+  ## NaN = dk/refused (-8, -9)
+
+## unemploymen increased/decreased: 
+  ## doesn't seem available in 2016 data
+
+## respondents race: V161310x
+## white, non-Hispanic (1) respondents only
+
+## blacks just need to try harder: V162214
+## 0 = disagree stongly (5)
+## .25 = disagree somewhat (4) 
+## .50 = Neither agree or disagree (3) 
+## .75 = agree somewhat (2)
+## 1 = strongly agree (1)
 
